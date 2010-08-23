bf0cdb860c1c790e905ee5a33e87bf9be35e11a9
Dylan Borg <borgdylan@hotmail.com>
2010-08-23 18:37:51 +0200

    dylan.NET new and old compiler

---
+++ compiler/make_dnc.vb
@@ -0,0 +1,266 @@
+﻿Imports tokenizer.Parser
+Imports tokenizer.AST.Stmts
+Imports tokenizer.Lexer
+Imports dnu
+Imports System.Linq
+Imports System.Collections.Generic
+Imports System.Collections
+Imports Microsoft.VisualBasic
+Imports System.Xml
+Imports System.Xml.Linq
+Imports System.Data
+
+Imports System
+Imports System.Diagnostics
+Imports System.Diagnostics.SymbolStore
+Imports System.Reflection
+Imports System.Resources
+Imports System.Reflection.Emit
+Module Module1
+
+Public asmName As AssemblyName
+Public asm As AssemblyBuilder
+Public Typ As Type
+Public Typ02 As Type
+Public Typ03 As Type
+Public Typ04 As Type
+Public impstr(-1) As String
+Public impasm(-1) As Assembly
+Public interfacebool As Boolean
+Public mdl As ModuleBuilder
+Public resw As IResourceWriter
+Public resobj As Object
+
+Dim doc As ISymbolDocumentWriter
+
+Sub addstr(ByVal str As String)
+ReDim Preserve impstr(UBound(impstr) + 1)
+impstr(UBound(impstr)) = str
+End Sub
+
+Sub addasm(ByVal asm As Assembly)
+ReDim Preserve impasm(UBound(impasm) + 1)
+impasm(UBound(impasm)) = asm
+End Sub
+
+Function MakeGetType(ByVal TypeName As String) As Type
+Dim attachbrackets As Boolean = False
+If TypeName Like "*[[]*]" Then
+Dim split As String() = TypeName.Split(New [Char] () {"[","]"})
+TypeName = split(0)
+attachbrackets = True
+End If
+Dim ind As Integer = -1
+Dim i As Integer = -1
+Dim len As Integer = impstr.Length - 1
+Do Until i = len
+i = i + 1
+If TypeName Like impstr(i) & "*" Then
+ind = i
+End If
+Loop
+If ind <> -1 Then
+Dim assem As Assembly = impasm(ind)
+If attachbrackets = True Then
+TypeName = TypeName & "[]"
+End If
+MakeGetType = assem.GetType(TypeName)
+Else
+If attachbrackets = True Then
+TypeName = TypeName & "[]"
+End If
+MakeGetType = Type.GetType(TypeName)
+End If
+Return MakeGetType
+End Function
+
+Dim doc2 As ISymbolDocumentWriter
+
+Sub Module1()
+Dim Module1 As TypeBuilder = mdl.DefineType("dylan.NET.Compiler" & "." & "Module1", TypeAttributes.Public Or TypeAttributes.AutoLayout Or TypeAttributes.AnsiClass, GetType(System.Object))
+Dim typ0(-1) As Type
+ReDim Preserve typ0(UBound(typ0) + 1)
+typ0(UBound(typ0)) = GetType(System.String).MakeArrayType()
+Dim main As MethodBuilder = Module1.DefineMethod("main", MethodAttributes.Public Or MethodAttributes.Static, GetType(System.Void), typ0)
+Dim mainIL As ILGenerator = main.GetILGenerator()
+Dim mainparam00 As ParameterBuilder = main.DefineParameter(0, ParameterAttributes.RetVal, "")
+Dim mainparam01 As ParameterBuilder = main.DefineParameter(1, ParameterAttributes.None, "args")
+mainIL.MarkSequencePoint(doc2, 5, 1, 5, 100)
+Dim typ1(-1) As Type
+mainIL.Emit(OpCodes.Ldstr, "dylan.NET Compiler v. 11.1 for Microsoft (R) .NET Framework (R) v. 3.5 SP1")
+Typ = GetType(System.String)
+ReDim Preserve typ1(UBound(typ1) + 1)
+typ1(UBound(typ1)) = Typ
+mainIL.Emit(OpCodes.Call, GetType(Console).GetMethod("WriteLine", typ1))
+Typ = GetType(Console).GetMethod("WriteLine", typ1).ReturnType
+If Typ.ToString() = GetType(System.Void).ToString() Then
+
+Else
+mainIL.Emit(OpCodes.Pop)
+End If
+mainIL.MarkSequencePoint(doc2, 6, 1, 6, 100)
+Dim typ2(-1) As Type
+mainIL.Emit(OpCodes.Ldstr, "                           and Novell Mono v. 2.6.7")
+Typ = GetType(System.String)
+ReDim Preserve typ2(UBound(typ2) + 1)
+typ2(UBound(typ2)) = Typ
+mainIL.Emit(OpCodes.Call, GetType(Console).GetMethod("WriteLine", typ2))
+Typ = GetType(Console).GetMethod("WriteLine", typ2).ReturnType
+If Typ.ToString() = GetType(System.Void).ToString() Then
+
+Else
+mainIL.Emit(OpCodes.Pop)
+End If
+mainIL.MarkSequencePoint(doc2, 7, 1, 7, 100)
+Dim typ3(-1) As Type
+mainIL.Emit(OpCodes.Ldstr, "Copyright (C) Dylan Borg 2010")
+Typ = GetType(System.String)
+ReDim Preserve typ3(UBound(typ3) + 1)
+typ3(UBound(typ3)) = Typ
+mainIL.Emit(OpCodes.Call, GetType(Console).GetMethod("WriteLine", typ3))
+Typ = GetType(Console).GetMethod("WriteLine", typ3).ReturnType
+If Typ.ToString() = GetType(System.Void).ToString() Then
+
+Else
+mainIL.Emit(OpCodes.Pop)
+End If
+mainIL.MarkSequencePoint(doc2, 8, 1, 8, 100)
+mainIL.Emit(OpCodes.Ldarg, 0)
+Typ = GetType(System.String).MakeArrayType()
+mainIL.Emit(OpCodes.Ldlen)
+mainIL.Emit(OpCodes.Conv_I4)
+Typ = GetType(System.Int32)
+mainIL.Emit(OpCodes.Ldc_I4, CInt(1))
+Typ = GetType(System.Int32)
+Dim fa0 As System.Reflection.Emit.Label = mainIL.DefineLabel()
+Dim tru0 As System.Reflection.Emit.Label = mainIL.DefineLabel()
+Dim cont0 As System.Reflection.Emit.Label = mainIL.DefineLabel()
+mainIL.Emit(OpCodes.Blt, tru0)
+mainIL.Emit(OpCodes.Br, fa0)
+mainIL.MarkLabel(tru0)
+mainIL.MarkSequencePoint(doc2, 9, 1, 9, 100)
+Dim typ4(-1) As Type
+mainIL.Emit(OpCodes.Ldstr, "Usage: dnc <path>")
+Typ = GetType(System.String)
+ReDim Preserve typ4(UBound(typ4) + 1)
+typ4(UBound(typ4)) = Typ
+mainIL.Emit(OpCodes.Call, GetType(Console).GetMethod("WriteLine", typ4))
+Typ = GetType(Console).GetMethod("WriteLine", typ4).ReturnType
+If Typ.ToString() = GetType(System.Void).ToString() Then
+
+Else
+mainIL.Emit(OpCodes.Pop)
+End If
+mainIL.MarkSequencePoint(doc2, 10, 1, 10, 100)
+mainIL.Emit(OpCodes.Br, cont0)
+mainIL.MarkLabel(fa0)
+mainIL.MarkSequencePoint(doc2, 14, 1, 14, 100)
+Dim locbldr0 As LocalBuilder = mainIL.DeclareLocal(GetType(System.String))
+locbldr0.SetLocalSymInfo("p")
+mainIL.Emit(OpCodes.Ldarg, 0)
+Typ = GetType(System.String).MakeArrayType()
+Typ02 = Typ
+mainIL.Emit(OpCodes.Ldc_I4, CInt(0))
+Typ = GetType(System.Int32)
+mainIL.Emit(OpCodes.Conv_U)
+Typ = Typ02
+mainIL.Emit(OpCodes.Ldelem, GetType(System.String).MakeArrayType().GetElementType())
+Typ = GetType(System.String).MakeArrayType().GetElementType()
+mainIL.Emit(OpCodes.Stloc, 0)
+mainIL.MarkSequencePoint(doc2, 15, 1, 15, 100)
+Dim locbldr1 As LocalBuilder = mainIL.DeclareLocal(GetType(Lexer))
+locbldr1.SetLocalSymInfo("lx")
+mainIL.Emit(OpCodes.Newobj, GetType(Lexer).GetConstructor(Type.EmptyTypes))
+mainIL.Emit(OpCodes.Stloc, 1)
+mainIL.MarkSequencePoint(doc2, 16, 1, 16, 100)
+Dim locbldr2 As LocalBuilder = mainIL.DeclareLocal(GetType(StmtSet))
+locbldr2.SetLocalSymInfo("pstmts")
+Dim typ5(-1) As Type
+mainIL.Emit(OpCodes.Ldloc, 1)
+Typ = GetType(Lexer)
+Typ03 = Typ
+mainIL.Emit(OpCodes.Ldloc, 0)
+Typ = GetType(System.String)
+ReDim Preserve typ5(UBound(typ5) + 1)
+typ5(UBound(typ5)) = Typ
+mainIL.Emit(OpCodes.Callvirt, Typ03.GetMethod("Analyze", typ5))
+Typ = Typ03.GetMethod("Analyze", typ5).ReturnType
+mainIL.Emit(OpCodes.Stloc, 2)
+mainIL.MarkSequencePoint(doc2, 17, 1, 17, 100)
+Dim locbldr3 As LocalBuilder = mainIL.DeclareLocal(GetType(Parser))
+locbldr3.SetLocalSymInfo("ps")
+mainIL.Emit(OpCodes.Newobj, GetType(Parser).GetConstructor(Type.EmptyTypes))
+mainIL.Emit(OpCodes.Stloc, 3)
+mainIL.MarkSequencePoint(doc2, 18, 1, 18, 100)
+Dim locbldr4 As LocalBuilder = mainIL.DeclareLocal(GetType(StmtSet))
+locbldr4.SetLocalSymInfo("ppstmts")
+Dim typ6(-1) As Type
+mainIL.Emit(OpCodes.Ldloc, 3)
+Typ = GetType(Parser)
+Typ03 = Typ
+mainIL.Emit(OpCodes.Ldloc, 2)
+Typ = GetType(StmtSet)
+ReDim Preserve typ6(UBound(typ6) + 1)
+typ6(UBound(typ6)) = Typ
+mainIL.Emit(OpCodes.Callvirt, Typ03.GetMethod("Parse", typ6))
+Typ = Typ03.GetMethod("Parse", typ6).ReturnType
+mainIL.Emit(OpCodes.Stloc, 4)
+mainIL.MarkSequencePoint(doc2, 27, 1, 27, 100)
+mainIL.Emit(OpCodes.Br, cont0)
+mainIL.MarkLabel(cont0)
+mainIL.MarkSequencePoint(doc2, 29, 1, 29, 100)
+mainIL.Emit(OpCodes.Ret)
+Module1.CreateType()
+asm.SetEntryPoint(main)
+Dim staType As Type = GetType(STAThreadAttribute)
+Dim staCtor As ConstructorInfo = staType.GetConstructor(Type.EmptyTypes)
+Dim staBuilder As CustomAttributeBuilder = New CustomAttributeBuilder(staCtor, New Object() {})
+main.SetCustomAttribute(staBuilder)
+
+End Sub
+
+Sub Main()
+
+asmName = New AssemblyName("dnc")
+asmName.Version = New System.Version(11, 1, 2, 0)
+asm  = AppDomain.CurrentDomain.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.Save, CStr("E:\Code\dylannet\compiler\"))
+mdl = asm.DefineDynamicModule(asmName.Name & ".exe" , asmName.Name & ".exe", True)
+resw = mdl.DefineResource("dnc.resources" ,  "Description")
+doc = mdl.DefineDocument("E:\Code\dylannet\compiler\dnc.txt", Guid.Empty, Guid.Empty, Guid.Empty)
+doc2 = mdl.DefineDocument("E:\Code\dylannet\compiler\Mod1.txt", Guid.Empty, Guid.Empty, Guid.Empty)
+addstr("dnc")
+addasm(asm)
+Dim daType As Type = GetType(DebuggableAttribute)
+Dim daCtor As ConstructorInfo = daType.GetConstructor(New Type() { GetType(DebuggableAttribute.DebuggingModes) })
+Dim daBuilder As CustomAttributeBuilder = New CustomAttributeBuilder(daCtor, New Object() {DebuggableAttribute.DebuggingModes.DisableOptimizations Or _
+DebuggableAttribute.DebuggingModes.Default })
+asm.SetCustomAttribute(daBuilder)
+
+Module1()
+Dim vaType As Type = GetType(AssemblyFileVersionAttribute)
+Dim vaCtor As ConstructorInfo = vaType.GetConstructor(New Type() { GetType(String) })
+Dim vaBuilder As CustomAttributeBuilder = New CustomAttributeBuilder(vaCtor, New Object() {"11.1.2.0"})
+asm.SetCustomAttribute(vaBuilder)
+
+Dim paType As Type = GetType(AssemblyProductAttribute)
+Dim paCtor As ConstructorInfo = paType.GetConstructor(New Type() { GetType(String) })
+Dim paBuilder As CustomAttributeBuilder = New CustomAttributeBuilder(paCtor, New Object() {"dnc"})
+asm.SetCustomAttribute(paBuilder)
+
+Dim ataType As Type = GetType(AssemblyTitleAttribute)
+Dim ataCtor As ConstructorInfo = ataType.GetConstructor(New Type() { GetType(String) })
+Dim ataBuilder As CustomAttributeBuilder = New CustomAttributeBuilder(ataCtor, New Object() {"dnc"})
+asm.SetCustomAttribute(ataBuilder)
+
+Dim deaType As Type = GetType(AssemblyDescriptionAttribute)
+Dim deaCtor As ConstructorInfo = deaType.GetConstructor(New Type() { GetType(String) })
+Dim deaBuilder As CustomAttributeBuilder = New CustomAttributeBuilder(deaCtor, New Object() {"dnc"})
+asm.SetCustomAttribute(deaBuilder)
+
+
+asm.DefineVersionInfoResource()
+asm.Save(asmName.Name & ".exe")
+End Sub
+
+
+End Module
---
+++ dylan.NET Compiler/dylan.NET Compiler/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("dylan.NET Compiler")> 
+<Assembly: AssemblyDescription("Compiler for dylan.NET")> 
+<Assembly: AssemblyCompany("Dylan Borg")> 
+<Assembly: AssemblyProduct("dylan.NET Compiler")> 
+<Assembly: AssemblyCopyright("Copyright ©  2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a5675d38-a7d2-4e02-ad9c-c48cc64b7029")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("9.9.0.0")> 
+<Assembly: AssemblyFileVersion("9.9.0.0")> 
