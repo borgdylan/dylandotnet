ecf5c7d10e5b15990a8a60641037a8b03a0026bf
Dylan Borg <borgdylan@hotmail.com>
2010-10-08 21:18:40 +0200

    removing everything from repo. please WAIT  for a new rep containin the latest updated packages,
    Signed-off-by: Dylan Borg <borgdylan@hotmail.com>

--- compiler/compile.bat
+++
@@ -1,5 +0,0 @@
-msbuild E:\Code\dylannet\compiler\tokenizer.AST.proj
-msbuild E:\Code\dylannet\compiler\tokenizer.Lexer.proj
-msbuild E:\Code\dylannet\compiler\tokenizer.Parser.proj
-msbuild E:\Code\dylannet\compiler\dnc.proj
-pause
